21915a109f01e8dfa7778978eff2c1e4f760fcd8
Isaac Keninger <isaac.keninger@gmail.com>
2025-08-05 17:34:10 -0500

    Update Main.py

--- app.py
+++ app.py
@@ -137,5 +137,4 @@ def update_todo():
     return jsonify({'message': 'Todo list updated successfully', 'todo': todo_list.to_dict()})
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True, port=5000)
