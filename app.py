1cdd91a62a1e98e70685074cd9c99ee7eca18a7b
Isaac Keninger <isaac.keninger@gmail.com>
2025-08-05 17:18:44 -0500

    Update Deployment

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 import json
+import os
 
 class toDo:
 
@@ -136,4 +137,5 @@ def update_todo():
     return jsonify({'message': 'Todo list updated successfully', 'todo': todo_list.to_dict()})
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
